cf06037998bcfb84e7b121676a5604a1b05e3dbb
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-03-30 23:04:28 +0500

    Add UI components for server list and empty server list
    - Create ServerList to display a list of servers
    - Create ServerListEmpty to display an empty list of servers

---
+++ app/src/main/java/com/example/webdavmanager/server_list/ui/component/ServerList.kt
@@ -0,0 +1,9 @@
+package com.example.webdavmanager.server_list.ui.component
+
+import androidx.compose.runtime.Composable
+import com.example.webdavmanager.server_list.domain.model.ServerItem
+
+@Composable
+fun ServerList(servers: List<ServerItem>) {
+    // TODO: Implement LazyColumn with server items
+}
---
+++ app/src/main/java/com/example/webdavmanager/server_list/ui/component/ServerListEmpty.kt
@@ -0,0 +1,8 @@
+package com.example.webdavmanager.server_list.ui.component
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ServerListEmpty() {
+    // TODO: Implement empty state UI with appropriate message
+}
