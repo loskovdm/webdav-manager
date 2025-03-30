f5d305939af0d2bd127d6323a937782586a481d9
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-03-30 23:11:40 +0500

    Move theme components to core module
    - Relocate Color, Theme, and Typography definitions to core.ui.theme

--- app/src/main/java/com/example/webdavmanager/MainActivity.kt
+++ app/src/main/java/com/example/webdavmanager/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.webdavmanager.ui.theme.WebdavManagerTheme
+import com.example.webdavmanager.core.ui.theme.WebdavManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/webdavmanager/core/ui/theme/Color.kt
+++ app/src/main/java/com/example/webdavmanager/core/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.webdavmanager.ui.theme
+package com.example.webdavmanager.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
