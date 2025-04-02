3e0f352a868ef3b63d3a7306d2df515809443fd1
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-04-02 12:32:39 +0500

    Add binding for ServerConfigRepository in ServerRepositoryModule

--- app/src/main/java/com/example/webdavmanager/core/di/ServerRepositoryModule.kt
+++ app/src/main/java/com/example/webdavmanager/core/di/ServerRepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.example.webdavmanager.core.di
 
 import com.example.webdavmanager.core.data.repository.ServerRepositoryImpl
+import com.example.webdavmanager.server_config.domain.repository.ServerConfigRepository
 import com.example.webdavmanager.server_list.domain.repository.ServerListRepository
 import dagger.Binds
 import dagger.Module
@@ -11,9 +12,17 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class ServerRepositoryModule {
+
     @Binds
     @Singleton
-    abstract fun bindServerRepository(
+    abstract fun bindServerListRepository(
         serverRepositoryImpl: ServerRepositoryImpl
     ): ServerListRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindServerConfigRepository(
+        serverRepositoryImpl: ServerRepositoryImpl
+    ): ServerConfigRepository
+
 }
