6a68df5e957f56596ecec02f516630436f57855d
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-04-03 22:53:31 +0500

    Remove unused imports in ServerConfigScreen for cleaner code

--- app/src/main/java/com/example/webdavmanager/server_config/ui/ServerConfigScreen.kt
+++ app/src/main/java/com/example/webdavmanager/server_config/ui/ServerConfigScreen.kt
@@ -1,13 +1,11 @@
 package com.example.webdavmanager.server_config.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.webdavmanager.server_config.ui.component.ErrorDialog
