9d6021a262d436337da8e67bc25ca333dbbadc53
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-04-06 17:35:24 +0500

    Add WebDavNavHost for navigation management between ServerList and ServerConfig screens

---
+++ app/src/main/java/com/example/webdavmanager/navigation/WebDavNavHost.kt
@@ -0,0 +1,52 @@
+package com.example.webdavmanager.navigation
+
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.webdavmanager.navigation.NavDestination.ServerConfigDestination
+import com.example.webdavmanager.navigation.NavDestination.ServerListDestination
+import com.example.webdavmanager.server_config.ui.ServerConfigScreen
+import com.example.webdavmanager.server_list.ui.ServerListScreen
+
+@Composable
+fun WebDavNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = ServerListDestination,
+        enterTransition = { EnterTransition.None },
+        exitTransition = { ExitTransition.None },
+        popEnterTransition = { EnterTransition.None },
+        popExitTransition = { ExitTransition.None }
+    ) {
+        composable<ServerListDestination> {
+            ServerListScreen(
+                onNavigateToServerConfig = { serverId ->
+                    navController.navigate(route = ServerConfigDestination(serverId = serverId))
+                }
+            )
+        }
+        composable<ServerConfigDestination> {
+            ServerConfigScreen(
+                onNavigateBack = {
+                    navController.popBackStack()
+                },
+                onNavigateBackWithChanges = {
+                    navController.navigate(ServerListDestination) {
+                        popUpTo(ServerListDestination) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
