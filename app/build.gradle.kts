d4f1da400fb83be5a2b0eebd8fde4bf737f21056
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-04-10 10:06:40 +0500

    Add sardine-android library and configure JitPack repository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.sardine.android)
+
 }
