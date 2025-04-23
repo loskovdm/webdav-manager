7db22601b64e2e41fb475997263a51baaed2492f
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-04-23 15:15:56 +0500

    Add OkHttpSardine provider to WebDavModule for dependency injection

--- app/src/main/java/com/example/webdavmanager/file_manager/di/WebDavModule.kt
+++ app/src/main/java/com/example/webdavmanager/file_manager/di/WebDavModule.kt
@@ -2,8 +2,10 @@ package com.example.webdavmanager.file_manager.di
 
 import com.example.webdavmanager.file_manager.data.remote.WebDavFileApiSardineImpl
 import com.example.webdavmanager.file_manager.data.remote.WebDavFileApi
+import com.thegrizzlylabs.sardineandroid.impl.OkHttpSardine
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -15,5 +17,13 @@ abstract class WebDavModule {
     @Binds
     @Singleton
     abstract fun bindWebDavFileApi(webDavFileApiSardineImpl: WebDavFileApiSardineImpl): WebDavFileApi
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideOkHttpSardine(): OkHttpSardine {
+            return OkHttpSardine()
+        }
+    }
 
 }
