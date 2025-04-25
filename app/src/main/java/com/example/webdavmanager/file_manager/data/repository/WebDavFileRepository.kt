6496541cd866185cb1a787be9a7a0edffc3decb0
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-04-25 20:23:51 +0500

    Add WebDavFileRepository interface for file management operations

---
+++ app/src/main/java/com/example/webdavmanager/file_manager/data/repository/WebDavFileRepository.kt
@@ -0,0 +1,54 @@
+package com.example.webdavmanager.file_manager.data.repository
+
+import com.example.webdavmanager.file_manager.data.model.WebDavConnectionInfo
+import com.example.webdavmanager.file_manager.data.model.WebDavFile
+import java.io.File
+import java.io.InputStream
+
+interface WebDavFileRepository {
+    suspend fun getFileList(
+        serverConnectionInfo: WebDavConnectionInfo,
+        directoryPath: String
+    ): Result<List<WebDavFile>>
+
+    suspend fun uploadFile(
+        serverConnectionInfo: WebDavConnectionInfo,
+        file: File,
+        directoryPath: String,
+        nameFile: String
+    ): Result<Unit>
+
+    suspend fun downloadFile(
+        serverConnectionInfo: WebDavConnectionInfo,
+        filePath: String
+    ): Result<InputStream>
+
+    suspend fun moveFile(
+        serverConnectionInfo: WebDavConnectionInfo,
+        filePath: String,
+        destinationDirectoryPath: String
+    ): Result<Unit>
+
+    suspend fun copyFile(
+        serverConnectionInfo: WebDavConnectionInfo,
+        filePath: String,
+        destinationDirectoryPath: String
+    ): Result<Unit>
+
+    suspend fun deleteFile(
+        serverConnectionInfo: WebDavConnectionInfo,
+        filePath: String
+    ): Result<Unit>
+
+    suspend fun createDirectory(
+        serverConnectionInfo: WebDavConnectionInfo,
+        destinationDirectoryPath: String,
+        name: String
+    ): Result<Unit>
+
+    suspend fun renameFile(
+        serverConnectionInfo: WebDavConnectionInfo,
+        file: String,
+        newName: String
+    ): Result<Unit>
+}
