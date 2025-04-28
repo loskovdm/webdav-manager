9ab56e1e8378f8d74a8c8d56a49203af0f59b4a4
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-04-28 20:26:43 +0500

    Add AndroidFileModule for dependency injection of application context

---
+++ app/src/main/java/com/example/webdavmanager/file_manager/di/AndroidFileModule.kt
@@ -0,0 +1,17 @@
+package com.example.webdavmanager.file_manager.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AndroidFileModule {
+    @Provides
+    @Singleton
+    fun provideContext(@ApplicationContext context: Context): Context = context
+}
