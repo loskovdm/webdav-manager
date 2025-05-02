240bd5d5826ec8ea32f761c4b9ba35c2634955c0
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-05-02 18:07:29 +0500

    Update FileItem data class to use uri and mimeType, and add isDirectory property

--- app/src/main/java/com/example/webdavmanager/file_manager/ui/model/FileItem.kt
+++ app/src/main/java/com/example/webdavmanager/file_manager/ui/model/FileItem.kt
@@ -4,8 +4,9 @@ import java.util.Date
 
 data class FileItem(
     val name: String,
-    val path: String,
-    val fileType: FileType,
+    val uri: String,
+    val mimeType: String?,
+    val isDirectory: Boolean,
     val size: Long?,
     val creationDate: Date?,
     val modifiedDate: Date?
