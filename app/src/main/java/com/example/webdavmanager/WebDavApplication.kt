e643b3d3d6c315e266ae3226ccf46a17664a0cff
Dmitry Loskov <loskovdm@users.noreply.github.com>
2025-05-02 15:08:13 +0500

    Implement activity lifecycle callbacks in WebDavApplication to clear cache on app background

--- app/src/main/java/com/example/webdavmanager/WebDavApplication.kt
+++ app/src/main/java/com/example/webdavmanager/WebDavApplication.kt
@@ -1,7 +1,39 @@
 package com.example.webdavmanager
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import com.example.webdavmanager.file_manager.data.repository.AndroidFileRepository
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
-class WebDavApplication : Application()
+class WebDavApplication : Application() {
+    @Inject
+    lateinit var androidFileRepository: AndroidFileRepository
+
+    private var activeActivities = 0
+
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+            override fun onActivityStarted(activity: Activity) { activeActivities++ }
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(p0: Activity) {
+                activeActivities--
+                if (activeActivities == 0) {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        androidFileRepository.clearCache()
+                    }
+                }
+            }
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
+    }
+}
